7df5a3125a7ac026e277c351bfdae7becd5e3833
neon-sunset <neon_sunset@outlook.com>
2023-08-12 06:46:59 +0300

    deps: update TwitchLib and Warpskimmer

--- dotnet/src/ComparisonBenchmarks.cs
+++ dotnet/src/ComparisonBenchmarks.cs
@@ -1,8 +1,8 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Jobs;
 using Warpskimmer;
-using forsen;
 using U8Primitives;
+using TwitchLib.Client.Parsing;
 
 namespace libs_comparison;
 
@@ -20,12 +20,11 @@ public class ComparisonBenchmarks
         {
             utf16Lines = File.ReadLines("data.txt").Take(1000).ToArray();
         }
-        catch (Exception e)
+        catch (FileNotFoundException)
         {
-            _ = e;
             utf16Lines = File.ReadLines("../../data.txt").Take(1000).ToArray();
         }
-        utf8Lines = utf16Lines.Select(line => line.ToU8String()).ToArray();
+        utf8Lines = utf16Lines.Select(U8String.Create).ToArray();
     }
 
     [Benchmark]
@@ -51,7 +50,7 @@ public class ComparisonBenchmarks
     {
         foreach (var line in utf16Lines)
         {
-            _ = TwitchLib.ParseIrcMessage(line);
+            _ = IrcParser.ParseMessage(line);
         }
     }
 }
