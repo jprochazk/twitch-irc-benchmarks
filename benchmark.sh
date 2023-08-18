49c6a50dab58fa3e6e04a947a06f209bf85f6a26
neon-sunset <neon_sunset@outlook.com>
2023-08-18 17:02:43 +0300

    fix: incorrect output folder in .sh

--- benchmark.sh
+++ benchmark.sh
@@ -1,18 +1,18 @@
 mkdir output
 
 echo "\nRunning Rust..."
-cd rust && cargo bench >> ../results/rust.txt && cd ..
+cd rust && cargo bench >> ../output/rust.txt && cd ..
 
 echo "\nRunning .NET..."
-cd dotnet/src && dotnet run -c Release >> ../../results/dotnet.txt && cd ../..
+cd dotnet/src && dotnet run -c Release >> ../../output/dotnet.txt && cd ../..
 
 echo "\nRunning Java..."
-cd jvm && ./gradlew clean && ./gradlew jmh >> ../results/java.txt && cd ..
+cd jvm && ./gradlew clean && ./gradlew jmh >> ../output/java.txt && cd ..
 
 echo "\nRunning Go..."
-cd go && go test -bench=. >> ../results/go.txt && cd ..
+cd go && go test -bench=. >> ../output/go.txt && cd ..
 
 echo "\nRunning Node.js..."
-cd node && npm i && node bench.js >> ../results/node.txt && cd ..
+cd node && npm i && node bench.js >> ../output/node.txt && cd ..
 
 # TODO: iterate through result files and parse them into a round summary
